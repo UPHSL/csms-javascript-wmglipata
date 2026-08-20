46db7598152ba449443896dadb2ffcfff4a8d341
Wilmar Lipata <wilmar.mg.lipata@gmail.com>
2026-08-20 13:56:10 +0800

    feat: define resident domain model

--- src/models/Resident.js
+++ src/models/Resident.js
@@ -3,5 +3,19 @@
  *
  * Resident behavior will be introduced through a future CSMS ticket.
  */
+
+export const ResidentStatus = {
+    ACTIVE: 'Active'
+};
+
 export class Resident {
+    constructor(id, firstName, lastName, address, contactNumber, email, status) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.contactNumber = contactNumber;
+        this.email = email;
+        this.status = status;
+    }
 }
